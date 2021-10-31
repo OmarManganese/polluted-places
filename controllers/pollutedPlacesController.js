2dc5c4660d8d619484aee4d36286e332a00519a2
OmarManganese <78893144+OmarManganese@users.noreply.github.com>
2021-10-31 11:16:01 +0100

    Added the controoler and route for the DELETE method

--- controllers/pollutedPlacesController.js
+++ controllers/pollutedPlacesController.js
@@ -1,4 +1,5 @@
 const PollutedPlace = require("../models/pollutedPlace");
+const fs = require('fs');
 
 const getAllPollutedPlaces = async (req, res) => {
   try {
@@ -22,9 +23,28 @@ const postPollutedPlace = async (req, res) => {
   }
 }
 
+const deletePollutedPlaceById = async (req,res) => {
+  try {
+    const id = req.params.id;
+    const pollutedPlace = await PollutedPlace.findById(id);
+    const {photoPath} = pollutedPlace;
+
+    await fs.unlink(photoPath, err => {
+      if (err) throw new Error(err);
+    })
+
+    await PollutedPlace.findByIdAndDelete(id);
+
+    res.status(201).send("Polluted place deleted successfully")
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+}
+
 
 
 module.exports = {
   getAllPollutedPlaces,
-  postPollutedPlace
+  postPollutedPlace,
+  deletePollutedPlaceById
 };
